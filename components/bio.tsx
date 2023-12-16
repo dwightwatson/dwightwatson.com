acd8c86f59e556c2b4f54c0d43c4264fd3dffa7b
Dwight Watson <dwightwatson@users.noreply.github.com>
2023-12-16 23:02:39 +1100

    Update bio

--- components/bio.tsx
+++ components/bio.tsx
@@ -5,49 +5,62 @@ export default function Bio() {
   return (
     <div className="border border-gray-300 rounded-md mb-8">
       <div className="flex justify-between items-center gap-5 p-4">
-        <p>
-          A blog about Laravel & Rails by Dwight Watson;
-          <br />
-          <span className="text-sm">
-            developer of{" "}
-            <a
-              href="https://www.roomies.com"
-              className="text-blue-700 underline"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Roomies.com
-            </a>
-            ,{" "}
-            <a
-              href="https://www.myrent.co.nz"
-              className="text-blue-700 underline"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              myRent.co.nz
-            </a>
-            ,{" "}
-            <a
-              href="https://highschoolnotes.com.au"
-              className="text-blue-700 underline"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              High School Notes
-            </a>{" "}
-            &amp;{" "}
-            <a
-              href="https://studentvip.com.au"
-              className="text-blue-700 underline"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              StudentVIP.com.au
-            </a>
-            .
-          </span>
-        </p>
+        <div>
+          <p>A blog about Laravel & Rails by Dwight Watson;</p>
+          <ul className="list-disc text-sm pl-4">
+            <li className="text-gray-700">
+              developer of{" "}
+              <a
+                href="https://www.roomies.com"
+                className="text-gray-700 underline"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Roomies
+              </a>
+              ,{" "}
+              <a
+                href="https://highschoolnotes.com.au"
+                className="text-gray-700 underline"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                High School Notes
+              </a>{" "}
+              &amp;{" "}
+              <a
+                href="https://studentvip.com.au"
+                className="text-gray-700 underline"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                StudentVIP
+              </a>
+              .
+            </li>
+            <li className="text-sm text-gray-500">
+              previously{" "}
+              <a
+                href="https://www.myrent.co.nz"
+                className="text-gray-500 underline"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                myRent
+              </a>{" "}
+              &amp;{" "}
+              <a
+                href="https://www.flatmates.com.au"
+                className="text-gray-500 underline"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Flatmates
+              </a>
+              .
+            </li>
+          </ul>
+        </div>
         <Image
           src={me}
           alt="Picture of Dwight Watson"
