facd6be6fe2c892be7b258c1ff2d1b021caa2600
Dwight Watson <dwightwatson@users.noreply.github.com>
2026-04-13 07:59:26 +1000

    Enable prefetch and use Astro fonts

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig } from "astro/config";
+import { defineConfig, fontProviders } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwindcss from "@tailwindcss/vite";
@@ -6,7 +6,24 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   site: "https://www.dwightwatson.com",
   trailingSlash: "never",
+  prefetch: {
+    prefetchAll: true,
+  },
   integrations: [mdx(), sitemap()],
+  fonts: [
+    {
+      provider: fontProviders.fontsource(),
+      name: "Atkinson Hyperlegible",
+      cssVariable: "--font-sans",
+      weights: [400, 700],
+    },
+    {
+      provider: fontProviders.fontsource(),
+      name: "League Spartan",
+      cssVariable: "--font-serif",
+      weights: [400],
+    },
+  ],
   vite: {
     plugins: [tailwindcss()],
   },
